3d3de9cb8837a134a620c4b3a4ef7ca8c880adf5
Melinda-ZXY <melinda.xinyi.zhang@gmail.com>
2023-04-16 15:55:23 -0400

    push dialogue_updated to unfamiliar

--- resources/df_up to unfamiliar.py
+++ resources/df_up to unfamiliar.py
@@ -59,7 +59,7 @@ transitions = {
         'state': 'start',
         '`Hi, I am dEPLoyer! Have you ever heard of Manchester United? `': {
             '[yes]': 'familiar',
-            'error': 'fun_fact'
+            '[no]': 'unfamiliar',
         },
     }
 
@@ -88,6 +88,51 @@ familiar = {
             },
         }
 
+
+unfamiliar={
+    'state':'unfamiliar',
+    '`Manchester United is part of EPL.  The Premier League was founded in 1992, '
+    'replacing the First Division as the top tier of English football. Some of the most '
+    'famous players you might have heard of are _, _, _. Does this sound interesting to you?`':{
+        '[yes]':{
+            '`Great! Manchester United is one of the most successful teams in the English Premier League. The famous player'
+            'Cristiano Ronaldo was once a member of Manchester United!`':{
+                '#GET_INTERESTED':{
+                    '$IF_Interested `Manchester United can be one of my favorite team. Do you have any favorite team in EPL so far?`':{
+                        '#GET_HOME_TEAM':{
+                            '`Good for you! `$home_team_ranking`.`':'match_discussion'
+                        },
+                        'error':{
+                        '`Oh that\'s fine. I watched Manchester United\'s recent game with Sevilla, another team in EPL. It was so intense! They got 2-2 eventually.`':{}
+                                '#GET_INTERESTED':{
+                                    '$IF_Interested `I know! Sabitzer from Manchester United had the first goal 14 minutes after the game for his team! '
+                                    'That is such a quick goal! Given that the average first goal for soccer game is after 30 minutes on average!`':{
+                                        '#GET_INTERESTED':{
+                                            '$IF_Interested':{
+                                                 '`Manchester United’s squad is one of the biggest in the Premier League and it’s filled up with quality players in every position. They are actually gonna have a game with Tottenham Hotspur soon.
+                                                 'They have long been rivals with each other and Hotspur currently ranks one below Manchester United!
+                                                 'Do you bother bet on their results?`':{
+                                                     '/*':{
+                                                         '#UNX, I would say 2-0. It somehow made me recall their game last year in October. They had 0-0 at half time.`':'end'
+                                                     },
+                                                 },
+                                            },
+                                            '$IF_NotInterested':'fun_fact'
+                                        }
+
+                                    }
+                                    }
+                                      '$IF_NotInterested':'fun_fact'
+                           }
+                        }
+                    '$IF_NotInterested'
+                    }
+                }
+            }
+        }
+    }
+
+}
 macros = {
         'GET_HOME_TEAM': MacroHome()
     }
